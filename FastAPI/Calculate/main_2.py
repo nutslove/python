b6ea1c91fa2389a25e7efa61f416ed93a08cf98b
nutslove <nuts_love86@yahoo.co.jp>
2022-04-18 04:18:29 +0900

    modified

---
+++ FastAPI/Calculate/main_2.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, Response
+from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse
+from typing import Optional
+from io import BytesIO
+from PIL import Image
+import httpx
+
+app = FastAPI()
+
+@app.get("/api/v1/calculate")
+def plus(operator:str = "", num_1: int = 10, num_2: int = 90): ## parameterとして定義したもの(ex. operator)は初期値を定義しておく必要がある。(でないとエラーになる)
+    if operator == "addition":
+        result = num_1 + num_2
+    elif operator == "multiplication":
+        result = num_1 * num_2
+    else:
+        result = 77
+
+    title = "犬と掛け算の部屋"
+    calculation = "掛け算"
+
+    db_url = "http://db.default.svc.cluster.local/dog/BauWow"
+    db = httpx.get(db_url)
+    return result, {"title": title, "calculation": calculation}, "dog", db.text
+
+@app.get("/api/v1/image/{animal}")
+def image_get(animal: str):
+    # image_url = "http://172.31.43.217:8080/api/v1/cat"
+    image_url = "http://image.default.svc.cluster.local/api/v1/" + animal
+    image = httpx.get(image_url)
+    image = Image.open(BytesIO(image.content))
+    image.save(f"static/{animal}.jpg")
+    image_file = f"static/{animal}.jpg"
+    return FileResponse(image_file)
--- FastAPI/Client/main.py
+++ FastAPI/Client/main.py
@@ -18,77 +18,62 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# @app.get("/")
-# async def root():
-#     return_message = httpx.get('http://172.31.32.49:8080/api/v1/first')
-#     print(return_message) # <Response [200 OK]>や<Response [404 Not Found]>など、ステータスコードが格納されている
-#     print(return_message.text) # 中身を取り出したいときは「.text」を付ける必要があるっぽい
-#     print(type(return_message))
-#     return "ID: " + return_message.text
-
-# @app.get("/param_int/{item_id}")
-# async def read_item(item_id: int): # FastAPIは自動でValidationチェックまでしてくれて、pythonでは型の強制力はなかった(int 以外の型が入ってもエラーにならない)気がするが、FastAPIの場合実際int以 外の型の値が入るとエラーになる。
-#     return {"int_item_id": item_id}
-
-# @app.get("/model/{model_name}")
-# async def model(model_name: ModelName): #上のModelNameクラスの下の"="の後ろに定義した値(このケースだと"Joonki","Su","Min")だけを受け付ける（それ以外はエラーとなる）
-#     if model_name == "Joonki":
-#         return {"LastName": "Lee"}
-#     else:
-#         return "I don't know that model."
+title = ""
+calculation = ""
+animal_type = ""
+name = ""
+breed = ""
+sex = ""
+age = ""
+owner = ""
 
 @app.get("/", response_class=HTMLResponse)
 async def plus(request: Request):
-#    title_url = "http://172.31.32.49:8080/api/v1/title?title=What Should I do?"
-    # title_url = "http://172.31.32.49:8080/api/v1/title"
-    title_url = "http://calculate.default.svc.cluster.local/api/v1/title"
-    title = httpx.get(title_url)
+
+    global title
+    global calculation
+    global animal_type
+    global name
+    global breed
+    global sex
+    global age
+    global owner
+
+    url = "http://calculate.default.svc.cluster.local/api/v1/calculate"
+    results = httpx.get(url)
     # print("Header: ", title.headers) # httpx.getで取得した「オブジェクト.headers」にHTTPヘッダの情報が入っている
     # print("URL: ", title.url) # httpx.getで取得した「オブジェクト.url」にURL情報が入ってる
     # print("Status Code: ", title) # httpx.getで取得したオブジェクトにはステータスコードが入ってる
-    jsondata = json.loads(title.text)
+    # cal_result = json.loads(results.text)[0] # getの時はcal_resultは使わない
+    jsondata = json.loads(results.text)[1]
     title = jsondata['title']
     calculation = jsondata['calculation']
     result = "数字を入力して下さい"
     # image_url = "http://172.31.32.49:8080/api/v1/image"
-    image_url = "http://calculate.default.svc.cluster.local/api/v1/image"
+    animal_type = json.loads(results.text)[2]
+    image_url = "http://calculate.default.svc.cluster.local/api/v1/image/" + animal_type
     image = httpx.get(image_url)
     image = Image.open(BytesIO(image.content))
-    image.save("static/animal.jpg")
-    # db_url = "http://18.182.63.40:8080/api/vi/db"
-    db_url = "http://calculate.default.svc.cluster.local/api/v1/db"
-    db = httpx.get(db_url)
-    animal = json.loads(db.text)
+    image.save(f"static/{animal_type}.jpg")
+    animal = json.loads(results.text)[3]
     name = json.loads(animal)['name']
     breed = json.loads(animal)['breed']
     sex = json.loads(animal)['sex']
     age = json.loads(animal)['age']
     owner = json.loads(animal)['owner']
 
-    return templates.TemplateResponse("root.html",{"request": request, "result": result, "title": title, "calculation": calculation, "name": name, "breed": breed, "sex": sex, "age": age, "owner": owner})
-
-## 2022/04/15 getとpost両方にDBからCatとDogの情報をとってきてname~owner変数に代入する事！
+    return templates.TemplateResponse("root.html",{"request": request, "result": result, "title": title, "calculation": calculation, "animal_type": animal_type, "name": name, "breed": breed, "sex": sex, "age": age, "owner": owner})
 
 @app.post("/", response_class=HTMLResponse)
 async def plus(request: Request, num1: int = Form(...), num2: int = Form(...)):
-    # url = f'http://172.31.32.49:8080/api/v1/calculate?num_1={num1}&num_2={num2}'
-    url = f'http://calculate.default.svc.cluster.local/api/v1/calculate?num_1={num1}&num_2={num2}'
-    result = httpx.get(url)
 
-    # title_url = "http://172.31.32.49:8080/api/v1/title"
-    title_url = "http://calculate.default.svc.cluster.local/api/v1/title"
-    title = httpx.get(title_url)
-    jsondata = json.loads(title.text)
-    title = jsondata['title']
-    calculation = jsondata['calculation']
-    # db_url = "http://18.182.63.40:8080/api/vi/db"
-    db_url = "http://calculate.default.svc.cluster.local/api/v1/db"
-    db = httpx.get(db_url)
-    animal = json.loads(db.text)
-    name = json.loads(animal)['name']
-    breed = json.loads(animal)['breed']
-    sex = json.loads(animal)['sex']
-    age = json.loads(animal)['age']
-    owner = json.loads(animal)['owner']
+    if animal_type == "cat":
+        operator = "addition"
+    elif animal_type == "dog":
+        operator = "multiplication"
+    url = f'http://calculate.default.svc.cluster.local/api/v1/calculate?operator={operator}&num_1={num1}&num_2={num2}'
+    results = httpx.get(url)
 
-    return templates.TemplateResponse("root.html",{"request": request, "result": result.text, "title": title, "calculation": calculation, "name": name, "breed": breed, "sex": sex, "age": age, "owner": owner})
+    result = json.loads(results.text)[0]
+
+    return templates.TemplateResponse("root.html",{"request": request, "result": result, "title": title, "calculation": calculation, "animal_type": animal_type, "name": name, "breed": breed, "sex": sex, "age": age, "owner": owner})
